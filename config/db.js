0f49ae64174f0644db625d3e477c758755ab8f6e
Akhilesh Pulingat <akhilesh0428@gmail.com>
2024-06-05 22:44:57 +0530

    fixing deployment error.

--- config/db.js
+++ config/db.js
@@ -3,12 +3,13 @@ const DB_URI = require('../constants/constants');
 
 const connectDB = async () => {
     try{
-        const connection = await mongoose.connect(DB_URI, {
+        const connection = await mongoose.connect('mongodb+srv://dba:dbAdministrator@cluster0.hj82hys.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
             serverApi: {
                 strict: true,
                 deprecationErrors: true,
             }
         });
+        
         console.log('MongoDB database connected.');
     }catch(error){
         console.log('Error in connecting database: ', error);
