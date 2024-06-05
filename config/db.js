3299e7e70b0e38363a1a23f4de62529459267d41
Akhilesh Pulingat <akhilesh0428@gmail.com>
2024-06-05 22:59:19 +0530

    error fixed in db.js

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ const DB_URI = require('../constants/constants');
 
 const connectDB = async () => {
     try{
+        const connection = await mongoose.connect('mongodb+srv://dba:dbAdministrator@cluster0.hj82hys.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     }catch(error){
         console.log('Error in connecting database: ', error);
         throw error;
