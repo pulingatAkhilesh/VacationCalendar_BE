e5616616f890248b35263c901f0f890fe66b75bc
Akhilesh Pulingat <akhilesh0428@gmail.com>
2024-06-05 22:44:57 +0530

    fixing deployment error.

--- config/db.js
+++ config/db.js
@@ -3,17 +3,18 @@ const DB_URI = require('../constants/constants');
 
 const connectDB = async () => {
     try{
-        const connection = await mongoose.connect(DB_URI, {
-            serverApi: {
-                strict: true,
-                deprecationErrors: true,
-            }
-        });
-        console.log('MongoDB database connected.');
+        // const connection = await mongoose.connect(DB_URI, {
+        //     serverApi: {
+        //         strict: true,
+        //         deprecationErrors: true,
+        //     }
+        // });
+        const connection = await mongoose.connect(DB_URI);
     }catch(error){
         console.log('Error in connecting database: ', error);
         throw error;
     };
+    console.log('MongoDB database connected.');
 };
 
 module.exports = connectDB;
