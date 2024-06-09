f532815744c17a7e16d085fc8e804dacbe328e94
Akhilesh Pulingat <akhilesh0428@gmail.com>
2024-06-09 22:45:37 +0530

    connection updated.

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const mongoURI = String(process.env.MONGO_URI);
 
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect('mongodb://127.0.0.1:27017/vacationcalendar', {
+        const connection = await mongoose.connect(mongoURI, {
         // const connection = await mongoose.connect('mongodb+srv://akhilesh0428:NQkPYRYED61g3EX8@vacationcalendar00.loon9k9.mongodb.net/', {
             useNewUrlParser: true,
             useUnifiedTopology: true
