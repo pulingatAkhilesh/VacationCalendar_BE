4a4e4480c82364644bf9dff9e2803775aedc1016
Akhilesh Pulingat <akhilesh0428@gmail.com>
2024-06-09 22:12:44 +0530

    npm vulnerabilities fixed.

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect('mongodb+srv://akhilesh0428:NQkPYRYED61g3EX8@vacationcalendar00.loon9k9.mongodb.net/', {
+        const connection = await mongoose.connect('mongodb://127.0.0.1:27017/vacationcalendar', {
+        // const connection = await mongoose.connect('mongodb+srv://akhilesh0428:NQkPYRYED61g3EX8@vacationcalendar00.loon9k9.mongodb.net/', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
