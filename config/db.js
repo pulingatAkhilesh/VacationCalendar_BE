36d193ebb927c3f50479bf7394b1d92739c263df
Akhilesh Pulingat <akhilesh0428@gmail.com>
2024-06-11 23:44:55 +0530

    enhancement in db connection.

--- config/db.js
+++ config/db.js
@@ -3,16 +3,14 @@ const mongoURI = String(process.env.MONGO_URI);
 
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(mongoURI, {
-        // const connection = await mongoose.connect('mongodb+srv://akhilesh0428:NQkPYRYED61g3EX8@vacationcalendar00.loon9k9.mongodb.net/', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+        await mongoose.connect(mongoURI, {
+            // No need for useNewUrlParser and useUnifiedTopology.
+        });
+        console.log('MongoDB database connected.');
     } catch (error) {
-        console.log('Error in connecting database: ', error);
-        throw error;
+        console.log('Error in connecting database: ', error.message);
+        process.exit(1);
     };
-    console.log('MongoDB database connected.');
 };
 
 module.exports = connectDB;
